d7e015be0d1ac839c2d6d6f5512bab46c0d006e8
Leon de Haas <96114373+LeondeHaas@users.noreply.github.com>
2022-02-21 15:33:55 +0100

    Add files via upload

---
+++ MiniCrud/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+       <header>
+    <nav>
+      <div class="header">
+        <div class="page">
+          <li><a class="pagina" href="index.php">Home</a></li>
+        </div>
+          <div class="page">
+               <li><a class="pagina" href="index.php">Menu</a></li>
+          </div>
+             <div class="page">
+               <li><a class="pagina" href="index.php">About us</a></li>
+          </div>
+                 <div class="page">
+               <li><a class="pagina" href="index.php">Contact</a></li>
+            </div>
+    </nav>
+  </header>
+ <div class="bg">
+  <img class="bg" src="Images/Kaniwa.jpg">
+</div>
+</body>
+</html>
