a9c173f0fbf19cc4f16ce1b9b297afc2f86171cc
Leon de Haas <96114373+LeondeHaas@users.noreply.github.com>
2022-03-10 09:00:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="styles.css">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-       <header>
-    <nav>
+  <head>
+    <link rel="stylesheet" href="styles.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
       <div class="header">
+        <img class="img" src="Images/sushi.png" alt="" />
         <div class="page">
           <li><a class="pagina" href="index.php">Home</a></li>
         </div>
-          <div class="page">
-               <li><a class="pagina" href="index.php">Menu</a></li>
-          </div>
-             <div class="page">
-               <li><a class="pagina" href="index.php">About us</a></li>
-          </div>
-                 <div class="page">
-               <li><a class="pagina" href="index.php">Contact</a></li>
-            </div>
-    </nav>
-  </header>
- <div class="bg">
-  <img class="bg" src="Images/Kaniwa.jpg">
-</div>
-</body>
+        <div class="page">
+          <li><a class="pagina" href="index.php">Menu</a></li>
+        </div>
+        <div class="page1">
+          <li><a class="pagina" href="index.php">About us</a></li>
+        </div>
+        <div class="page">
+          <li><a class="pagina" href="index.php">Contact</a></li>
+        </div>
+      </div>
+    </header>
+    <div class="container"></div>
+  </body>
+  <footer>
+    <div class="footer">
+      <div class="block1">
+        <h1>Location</h1>
+        <h3>Heijdbloemstraat 65</h3>
+        <h3> 5345 GB Nijmegen</h3>
+        <h3> 024-9447291</h3>
+      </div>
+     <div class="containertime">
+       <div class="block">
+         <h1>Delivery</h1>
+         <h3>Monday</h3>
+         <h3>Tuesday</h3>
+         <h3>Wednesday</h3>
+         <h3>Thursday</h3>
+         <h3>Friday</h3>
+         <h3>Saturday</h3>
+         <h3>Sunday</h3>
+       </div>
+       <div class="block">
+          <h1>Times</h1>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+       </div>
+     </div>
+     <div class="containertime">
+       <div class="block">
+         <h1>Pickup</h1>
+         <h3>Monday</h3>
+         <h3>Tuesday</h3>
+         <h3>Wednesday</h3>
+         <h3>Thursday</h3>
+         <h3>Friday</h3>
+         <h3>Saturday</h3>
+         <h3>Sunday</h3>
+       </div>
+       <div class="block">
+          <h1>Times</h1>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+         <h3>09:00 - 22:00</h3>
+       </div>
+     </div>
+    
+    <div class="tm"></div>
+  </footer>
 </html>
